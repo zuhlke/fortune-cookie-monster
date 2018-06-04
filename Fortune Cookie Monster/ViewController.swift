9ad725f396e602445450dd68c4861f043f3e19dc
Jonathan Rothwell <Jonathan.Rothwell@zuhlke.com>
2018-06-04 12:14:30 +0200

    Initial Commit

---
+++ Fortune Cookie Monster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fortune Cookie Monster
+//
+//  Created by Jonathan Rothwell on 04/06/2018.
+//  Copyright © 2018 Zuhlke UK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Fortune Cookie MonsterTests/Fortune_Cookie_MonsterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fortune_Cookie_MonsterTests.swift
+//  Fortune Cookie MonsterTests
+//
+//  Created by Jonathan Rothwell on 04/06/2018.
+//  Copyright © 2018 Zuhlke UK. All rights reserved.
+//
+
+import XCTest
+@testable import Fortune_Cookie_Monster
+
+class Fortune_Cookie_MonsterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
